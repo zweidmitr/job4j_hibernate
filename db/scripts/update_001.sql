4a17f54f26c8b15cc4dce19be87d229fec53ff47
zweidmitr <zweidmitr@gmail.com>
2022-06-05 11:43:13 +0300

    0. ToOne lesson

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -2,4 +2,17 @@ create table if not exists candidates
 (
     id   serial primary key,
     name varchar(255)
-)
+);
+
+create table j_role
+(
+    id   serial primary key,
+    name varchar(2000)
+);
+
+create table j_user
+(
+    id      serial primary key,
+    name    varchar(2000),
+    role_id int not null references j_role (id)
+);
---
+++ src/main/java/hiber/toone/User.java
@@ -0,0 +1,65 @@
+package hiber.toone;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "j_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public static User of(String name, Role role) {
+        User user = new User();
+        user.name = name;
+        user.role = role;
+        return user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return user.id == id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
